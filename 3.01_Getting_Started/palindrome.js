1b2cb0e211dc786dc18b86697cd94e9f792c85ee
vladharagea <vladharagea@yahoo.com>
2019-07-11 11:37:07 +0200

    Managed to built a function that checks if a word is a palindrome

--- 3.01_Getting_Started/palindrome.js
+++ 3.01_Getting_Started/palindrome.js
@@ -1,21 +1,47 @@
 
-function checkIfPalindrome () {
+function checkIfPalindrome (phrase) {
 
     // we take the string 
         //we have done that = cool!
     //we make it lowercase or uppercase
-        transformToLowerCase(phrase);
+    let lowerPhrase = transformToLowerCase(phrase);
     //we produce the reversed string
-        reversePhrase(phrase);
+    let reversedPhrase =  reversePhrase(phrase);
     //we check if original  and reverse strings are identical
-        comparePhrase(phrase)
+    let res = comparePhrase(lowerPhrase, reversedPhrase);
+    console.log(`The word "${phrase}" is a palindrome: ${res}`)
+}
 
+function transformToLowerCase(str) {
+    let lc = str.toLowerCase();
+    return lc;
+}
+
+function reversePhrase(sent) {
+    let res = '';
+    let l = sent.length - 1;
+
+    for(let i=l;  i >= 0; i--) {
+        res = res + sent[i]
+    }
+    return res;
+}
+
+function comparePhrase(low, rev) {
+    if(low == rev) {
+        return true;
+    } 
+    else {
+        return false;
+    };
+    //let res = (low == rev) ? true : false
+    //return res
 }
 
 
-
 //Our Tests
-checkIfPalindrome('level') // Return true
-checkIfPalindrome('mAdaM') //Return true, but send a message that complains about the input
-checkIfPalindrome('babylon') // Return false
-// checkIfPalindrome('i level madam level i') // Return true
+checkIfPalindrome('level'); // Return true
+checkIfPalindrome('mAdaM'); //Return true, but send a message that complains about the input
+checkIfPalindrome('babylon'); // Return false
+checkIfPalindrome('i level madam level i') // Return true
+// checkIfPalindrome(33445);
