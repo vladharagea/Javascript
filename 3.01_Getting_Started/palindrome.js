ab68052a030b67e7c65a23c097d3e56367ca3a4e
vladharagea <vladharagea@yahoo.com>
2019-07-11 10:04:02 +0200

    started resolving the palindrome again with more clear functions

---
+++ 3.01_Getting_Started/palindrome.js
@@ -0,0 +1,21 @@
+
+function checkIfPalindrome () {
+
+    // we take the string 
+        //we have done that = cool!
+    //we make it lowercase or uppercase
+        transformToLowerCase(phrase);
+    //we produce the reversed string
+        reversePhrase(phrase);
+    //we check if original  and reverse strings are identical
+        comparePhrase(phrase)
+
+}
+
+
+
+//Our Tests
+checkIfPalindrome('level') // Return true
+checkIfPalindrome('mAdaM') //Return true, but send a message that complains about the input
+checkIfPalindrome('babylon') // Return false
+// checkIfPalindrome('i level madam level i') // Return true
